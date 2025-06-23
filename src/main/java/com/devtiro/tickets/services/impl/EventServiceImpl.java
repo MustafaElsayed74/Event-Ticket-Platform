82a1512223679e0711cd32802384c835f4b16691
0x0desha74 <mudesha74@gmail.com>
2025-06-23 23:46:10 +0300

    getEventForOrganizer service

--- src/main/java/com/devtiro/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/com/devtiro/tickets/services/impl/EventServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -64,4 +65,11 @@ public class EventServiceImpl implements EventService {
 
 
     }
+
+    @Override
+    public Optional<Event> getEventForOrganizer(UUID organizerId, UUID id) {
+        return eventRepository.findByIdAndOrganizerId(id,organizerId);
+    }
+
+
 }
