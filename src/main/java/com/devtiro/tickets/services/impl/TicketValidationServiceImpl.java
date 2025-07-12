b50465ff9d87f89c6a0454dfe90e8644ca1a9902
0x0desha74 <mudesha74@gmail.com>
2025-07-12 22:34:23 +0300

    ValidateTicketManually Service

--- src/main/java/com/devtiro/tickets/services/impl/TicketValidationServiceImpl.java
+++ src/main/java/com/devtiro/tickets/services/impl/TicketValidationServiceImpl.java
@@ -7,6 +7,7 @@ import com.devtiro.tickets.domain.enums.QrCodeStatusEnum;
 import com.devtiro.tickets.domain.enums.TicketValidationMethodEnum;
 import com.devtiro.tickets.domain.enums.TicketValidationStatusEnum;
 import com.devtiro.tickets.exceptions.QrCodeNotFoundException;
+import com.devtiro.tickets.exceptions.TicketNotFoundException;
 import com.devtiro.tickets.repositories.QrCodeRepository;
 import com.devtiro.tickets.repositories.TicketRepository;
 import com.devtiro.tickets.repositories.TicketValidationRepository;
@@ -57,6 +58,26 @@ public class TicketValidationServiceImpl implements TicketValidationService {
 
     @Override
     public TicketValidation validateTicketManually(UUID ticketId) {
-        return null;
+        Ticket ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException(
+                        String.format(
+                                "Ticket By ID %s Was Not Found", ticketId
+                        )
+                ));
+
+        TicketValidation ticketValidation = new TicketValidation();
+
+        ticketValidation.setTicket(ticket);
+        ticketValidation.setValidationMethod(TicketValidationMethodEnum.MANUAL);
+
+        TicketValidationStatusEnum status = ticket.getValidations().stream()
+                .filter(v -> TicketValidationStatusEnum.VALID.equals(v.getStatus()))
+                .findFirst()
+                .map(v -> TicketValidationStatusEnum.INVALID)
+                .orElse(TicketValidationStatusEnum.VALID);
+
+        ticketValidation.setStatus(status);
+
+        return ticketValidationRepository.save(ticketValidation);
     }
 }
